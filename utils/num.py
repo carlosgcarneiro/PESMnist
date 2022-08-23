5e6c27fbea64856bd3daf8fc4500899171699683
André B <balieiro1396@gmail.com>
2022-08-23 15:11:17 -0300

    refact

--- utils/num.py
+++ utils/num.py
@@ -4,16 +4,18 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 
-class TestNum:
+class Num:
     
-    def __init__(self, path: str) -> None:
+    def __init__(self, path: str, invert: bool = False) -> None:
         self.img_path = path
         self.img = np.asarray(Image.open(path))
-        
-    def print_number(self, invert: bool = False) -> None:
+
         if invert:
-            img = np.invert(img)
+            self.img = np.invert(self.img)
         
+    def plot(self) -> None:
         plt.imshow(self.img, cmap='gray', vmin=0, vmax=255)
         plt.show()
-        
+    
+    def to_array(self) -> np.array:
+        return np.reshape(self.img, (1,-1))    
