e589b9bae9f00e79ee97c0a61f6862a0eeada904
André B <balieiro1396@gmail.com>
2022-08-24 12:46:11 -0300

    feat: add a class to experiment multiple models at once

---
+++ utils/run_models.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, plot_confusion_matrix
+
+
+class RunModels:
+    
+    def __init__(self, models: dict) -> None:
+        self.models = models
+    
+    def load_data(self, X: pd.DataFrame, y: pd.Series) -> None:
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X,y)
+        
+    def train(self) -> None:
+        self.trained_models = dict()
+        self.preds = dict()
+        for name, model in self.models.items():
+            self.trained_models[name] = model.fit(self.X_train, self.y_train)
+            self.preds[name] = model.predict(self.X_test)
+    
+    def report(self, models: list[str] = None) -> None:
+        if not models:
+            models = self.models
+
+        for model in models:
+            print(model + '\n')
+            print(classification_report(self.y_test, self.pred[model]))
+        
+    
+    def conf_matrix(self, models: list[str] = None) -> None:
+        if not models:
+            models = self.models
+        
+        for model in models:
+            print(model + '\n')
+            plot_confusion_matrix(self.train[model], self.X_test, self.y_test)
+
+            
+        
+            
+    
+        
