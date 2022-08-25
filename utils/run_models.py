ea1899979d2ec54922d8be48e85c6f042e96e2e9
André B <balieiro1396@gmail.com>
2022-08-25 09:57:41 -0300

    feat: add scores attr to RunModels class; refacts

--- utils/run_models.py
+++ utils/run_models.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, plot_confusion_matrix
+from sklearn.metrics import classification_report
 
 
 class RunModels:
     
     def __init__(self, models: dict) -> None:
         self.models = models
+        self.scores = dict()
     
-    def load_data(self, X: pd.DataFrame, y: pd.Series) -> None:
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X,y)
-        
+    def load_data(self, X_train: pd.DataFrame, X_test: pd.Series,
+                  y_train: pd.DataFrame, y_test: pd.Series) -> None:
+        self.X_train, self.X_test = X_train, X_test
+        self.y_train, self.y_test = y_train, y_test
+
     def train(self) -> None:
         self.trained_models = dict()
         self.preds = dict()
@@ -19,25 +21,23 @@ class RunModels:
             self.trained_models[name] = model.fit(self.X_train, self.y_train)
             self.preds[name] = model.predict(self.X_test)
     
+    def accuracy(self, models: list[str] = None) -> dict:
+        if not models:
+            models = self.models.keys()
+
+        for model in models:
+            self.scores[model] = self.trained_models[model].score(
+                self.X_test,
+                self.y_test,
+            )
+        
+        return self.scores
+
     def report(self, models: list[str] = None) -> None:
         if not models:
-            models = self.models
+            models = self.models.keys()
 
         for model in models:
             print(model + '\n')
             print(classification_report(self.y_test, self.preds[model]))
-        
-    
-    def conf_matrix(self, models: list[str] = None) -> None:
-        if not models:
-            models = self.models
-        
-        for model in models:
-            print(model + '\n')
-            plot_confusion_matrix(self.trained_models[model], self.X_test, self.y_test)
-
-            
-        
-            
-    
         
